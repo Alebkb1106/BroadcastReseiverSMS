897e0275e5c843bc811d32a46d7a692ece3f64ca
Alebkb1106 <s18120161@alumnos.itsur.edu.mx>
2023-03-10 08:01:12 -0600

    mensajes jalando

--- app/src/main/java/com/example/broadcastreseiversms/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/broadcastreseiversms/MyBroadcastReceiver.kt
@@ -19,12 +19,21 @@ class MyBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
             val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
-            if (state == TelephonyManager.EXTRA_STATE_RINGING) {
-                var phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                var phonehg="4451455052"
-                if (phonehg == getAutoReplyNumber(context)) {
-                    sendAutoReplySms(context)
+
+            if (state != null) {
+                if (state == TelephonyManager.EXTRA_STATE_RINGING) {
+                    var phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                    Log.d("MENSAJES Y LLAMADAS",intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).toString())
+
+                    phoneNumber = phoneNumber.toString().substring(3)
+                    Log.d("NUMBER MESSAGE",phoneNumber)
+                    if (phoneNumber == getAutoReplyNumber(context).toString()&& phoneNumber.toString()!=null) {
+
+                        Log.d("INSIDE MESSAGE","Estaaaa dentrooooo")
+                        sendAutoReplySms(context)
+                    }
                 }
+
             }
         }
     }
